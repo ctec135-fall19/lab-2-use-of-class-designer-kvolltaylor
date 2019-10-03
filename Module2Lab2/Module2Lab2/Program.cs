7b23bdbf9ee3c9ac720cd347d5109556ddb0f6b5
Katrina <k.voll-taylor@students.clark.edu>
2019-10-03 09:49:35 -0700

    Lab2Submission - second try

---
+++ Module2Lab2/Module2Lab2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2Lab2
+{
+    class Program
+    {
+        private int PetName;
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Module2Lab2/Module2Lab2/SportsCar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Module2Lab2
+{
+    public class SportsCar : Cars
+    {
+        public string GetPetName()
+        {
+            GetPetName = "Fred";
+            return GetPetName;
+        }
+    }
+}
